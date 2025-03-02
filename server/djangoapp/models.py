a03322f3c0f7f0e555e62fa87d1610fd87c4a4d5
bryanjfehr <bryanjfehr@gmail.com>
2025-03-02 02:51:32 -0700

    Ran delinting and formatting on all applicable files.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
-from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+
 # Create your models here.
-
-
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -16,16 +16,23 @@ class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        ('PICKUP', 'Pickup')
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
+        ("PICKUP", "Pickup"),
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2025, validators=[
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default="SUV"
+    )
+    year = models.IntegerField(
+        default=2025,
+        validators=[
             MaxValueValidator(2025),
-            MinValueValidator(1950)
-        ])
+            MinValueValidator(1950),
+        ]
+    )
 
     def __str__(self):
         return self.name  # Return the name as the string representation
